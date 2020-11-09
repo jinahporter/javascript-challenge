ca0d5a8414a665114451266c45012f9cf74b7a29
Jinah Porter <jinahporter@gmail.com>
2020-11-09 13:09:05 -0600

    display data

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,6 +2,29 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+var tbody = d3.select("tbody");
+//console.log(tbody);
+
+console.log(tableData);
+
+tableData.forEach(resultsUFO => {
+    //console.log(resultsUFO);
+
+    //use d3 to append one table row "tr" for each data results
+    var row = tbody.append("tr");
+
+    //use Object.entries to log each report value 
+    Object.values(resultsUFO).forEach(value => {
+        console.log(value);
+        
+        //use d3 to append 1 cell per report
+        var cell = row.append("td");
+
+        cell.text(value);
+    });
+});
+
 var button = d3.select("#filter-btn");
 
 button.on("click", function() {
@@ -9,12 +32,12 @@ button.on("click", function() {
     var inputElement = d3.select("#datetime");
     var inputValue = inputElement.property("value");
 
-    console.log(inputValue);
-    console.log(tableData);
+    //console.log(inputValue);
+    //console.log(tableData);
 
-    var results = tableData.filter(date => date.datetime === inputValue);
+    var filteredresults = tableData.filter(date => date.datetime === inputValue);
 
-    console.log(results);
+    console.log(filteredresults);
 
     //d3.select("")
 })
